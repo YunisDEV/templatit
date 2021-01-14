2dc73fcd8b0541a22c48d112176ff78482c1e7a0
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-01-14 23:59:57 +0400

    demo

--- templatit.js
+++ templatit.js
@@ -1,7 +1,7 @@
 function templatit(selector,data={}){
-    let tstr = document.querySelector(selector).innerHTML
+    var tstr = document.querySelector(selector).innerHTML
     Object.keys(data).forEach(key=>{
-        tstr.replaceAll(`{${key}}`,data[key])
+        tstr = tstr.replaceAll(`{${key}}`,data[key])
     })
     return tstr
 }

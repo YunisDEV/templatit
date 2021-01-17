37f14c4a21df9d9c5a92c7cad424b6c48ef26bb3
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-01-17 12:56:01 +0400

    OOP based way

--- templatit.js
+++ templatit.js
@@ -1,7 +1,30 @@
-function templatit(selector,data={}){
-    var tstr = document.querySelector(selector).innerHTML
-    Object.keys(data).forEach(key=>{
-        tstr = tstr.replaceAll(`{${key}}`,data[key])
+function templatit(selector, data = {}, templateString) {
+    var tstr = null
+    if (selector) var tstr = document.querySelector(selector).innerHTML
+    else var tstr = templateString
+    Object.keys(data).forEach(key => {
+        tstr = tstr.replaceAll(`{${key}}`, data[key])
     })
     return tstr
+}
+
+class TemplatitBase {
+    constructor(tempStr = null) {
+        if (tempStr) this.temp_str = tempStr
+    }
+    template() {
+        let {temp_str=null} = this
+        return temp_str.trim()
+    }
+
+    render({ data = {}, where = null, callback = null } = {}) {
+        var tempStr = templatit(null, data, this.template())
+        if (where) {
+            document.querySelector(where).innerHTML = tempStr
+        }
+        if (callback) {
+            callback(tempStr)
+        }
+        return tempStr
+    }
 }
